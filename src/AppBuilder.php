d04741e2a576e9764669eb5ec2dd8724a6f8cfb2
Florian Röpstorf <florian.roepstorf@gmail.com>
2022-02-18 17:15:40 +0100

    Let slim parse the incoming body and adds purchase route

--- src/AppBuilder.php
+++ src/AppBuilder.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace Froepstorf\Cryptoportfolio;
 
 use DI\Bridge\Slim\Bridge;
+use Froepstorf\Cryptoportfolio\Controllers\PurchaseController;
 use Froepstorf\Cryptoportfolio\Controllers\RootController;
 use Slim\App;
 
@@ -13,7 +14,10 @@ class AppBuilder
     {
         $app = Bridge::create($containerBuilder->build(EnvironmentReader::getAppEnvironment()));
 
+        $app->addBodyParsingMiddleware();
+
         $app->get('/', [RootController::class, 'root']);
+        $app->post('/purchase', [PurchaseController::class, 'registerPurchase']);
 
         return $app;
     }
