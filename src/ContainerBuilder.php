220dd60116e05d034ae7d033d1402f2f24646885
Florian Röpstorf <florian.roepstorf@gmail.com>
2022-02-18 17:14:56 +0100

    Early return if prod needs to build

--- src/ContainerBuilder.php
+++ src/ContainerBuilder.php
@@ -16,6 +16,7 @@ class ContainerBuilder
     {
         if ($appEnvironment->isProd()) {
             $this->forProd();
+            return $this->builder->build();
         }
 
         return $this->builder->build();
