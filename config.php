8842a176dd60504b055285a25d81b792ec998ceb
Florian Röpstorf <florian.roepstorf@gmail.com>
2022-02-18 18:28:46 +0100

    Adds moneyphp

--- config.php
+++ config.php
@@ -2,8 +2,12 @@
 declare(strict_types=1);
 
 use Froepstorf\Cryptoportfolio\AppEnvironment;
+use Froepstorf\Cryptoportfolio\Domain\SupportedCurrencies;
 use Froepstorf\Cryptoportfolio\EnvironmentReader;
 use Froepstorf\Cryptoportfolio\Services\PurchaseService;
+use Money\Currencies\ISOCurrencies;
+use Money\Formatter\AggregateMoneyFormatter;
+use Money\Formatter\IntlMoneyFormatter;
 use Monolog\Handler\StreamHandler;
 use Monolog\Logger;
 use Psr\Log\LoggerInterface;
@@ -24,7 +28,22 @@ return [
         return $logger;
     },
 
-    PurchaseService::class => function (LoggerInterface $logger): PurchaseService {
-        return new PurchaseService($logger);
+    PurchaseService::class => function (LoggerInterface $logger, AggregateMoneyFormatter $moneyFormatter): PurchaseService {
+        return new PurchaseService($logger, $moneyFormatter);
+    },
+
+    NumberFormatter::class => function(): NumberFormatter {
+        return new NumberFormatter('de_DE', NumberFormatter::CURRENCY);
+    },
+
+    IntlMoneyFormatter::class => function (NumberFormatter $numberFormatter): IntlMoneyFormatter {
+        return new IntlMoneyFormatter($numberFormatter, new ISOCurrencies());
+    },
+
+    AggregateMoneyFormatter::class => function (IntlMoneyFormatter $intlMoneyFormatter): AggregateMoneyFormatter {
+        return new AggregateMoneyFormatter([
+            SupportedCurrencies::USD->value => $intlMoneyFormatter,
+            SupportedCurrencies::EUR->value => $intlMoneyFormatter
+        ]);
     }
 ];
